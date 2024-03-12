66319d210f56bb1b6cb202b43c4a94008034b928
Paulo <pmarquesbaronefilho18@protonmail.com>
2024-03-12 16:18:11 -0300

    fix: download pdf button

--- src/utils/DownloadPDF.tsx
+++ src/utils/DownloadPDF.tsx
@@ -1,12 +1,16 @@
 import pdfContent from '../assets/kim-freitas-cv.pdf'
 
 export default function DownloadPDF() {
-  const blob = new Blob([pdfContent], { type: 'application/pdf' })
-
-  const link = document.createElement('a')
-  link.href = URL.createObjectURL(blob)
-  link.download = 'kim-freitas-cv.pdf'
-  link.click()
-
-  URL.revokeObjectURL(link.href)
+  fetch(pdfContent)
+    .then(response => response.blob())
+    .then(blob => {
+      const link = document.createElement('a')
+      link.href = URL.createObjectURL(blob)
+      link.download = 'kim-freitas-cv.pdf'
+      link.click()
+      URL.revokeObjectURL(link.href)
+    })
+    .catch((error) => {
+      console.error('Erro ao carregar o PDF:', error)
+    })
 }
